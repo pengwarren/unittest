667a720c60b9a568f68cc238ee2ac495c90d4dcf
pengwarren <53215092+pengwarren@users.noreply.github.com>
2019-11-26 21:04:42 +0800

    Add files via upload

--- smartroomtest.py
+++ smartroomtest.py
@@ -26,6 +26,91 @@ class Smartroom(object):
     def build_training_data(self):
         return [(text, label) for label, texts in self.configurations["COMMANDS"]["PHRASES"].items() for text in texts]
 
+    def perform_classification(self, is_naive=False):
+        self.state = self.NLPM
+        verbs = self.verbs
+        polarities = self.polarities
+
+        if set(["n't", "not"]).intersection(self.verbs):
+            verbs = list(self.verbs)
+            polarities = list(self.polarities)
+            try:
+                i = self.verbs.index("n't")
+            except ValueError:
+                i = self.verbs.index("not")
+            verbs.pop(i)
+            polarities.pop(i)
+            verbs[i] = f"!{verbs[i]}"
+
+            if is_naive:
+                polarities[i] = 0 if self.classifier.classify(self.text) > 0 else 1
+            else:
+                polarities[i] = 0 if polarities[i] > 0 else 1
+        elif is_naive:
+            self.polarities = [self.classifier.classify(self.text)]
+
+        self.state = self.IDLE
+        return self.__response__(verbs=verbs, polarities=polarities)
+
+    def perform_naive_bayes_classification(self):
+        return self.perform_classification(is_naive=True)
+
+    def perform_request(self, method, link, payload=None, key=None):
+        try:
+            response = method(
+                f"{self.configurations['KNX_BAOS_SERVER']['URL']}{link}",
+                cookies=key,
+                data=Json.dumps(payload)
+            )
+        except Exception:
+            raise NotImplementedError
+
+        self.verify_status_code(response)
+        return response
+
+    def wait_for_wake_word(self, wake_word):
+        while self.convert_speech_to_text() != wake_word.lower():
+            self.state = self.IDLE
+
+        self.state = self.WAKE
+        return self.convert_speech_to_text()
+
+    @classmethod
+    def verify_status_code(cls, response):
+        if response.status_code not in (Requests.codes.ok, Requests.codes.no_content):
+            response.raise_for_status()
+
+    @property
+    def words(self):
+        self._words = Text(self.text).words
+        return self._words
+
+    def build_training_data(self):
+        return [
+            (text, label)
+            for label, texts in self.configurations["COMMANDS"]["PHRASES"].items()
+            for text in texts
+        ]
+
+    def convert_speech_to_text(self):
+        try:
+            with self.microphone as input:
+                self.recognizer.adjust_for_ambient_noise(input)
+                speech = self.recognizer.listen(input)
+            self.state = self.ASRM
+            self.text = self.recognizer.recognize_google(speech)
+            self.state = self.IDLE
+            return self.text
+        except Speech.RequestError:
+            print(f"{self.__class__.__name__} failed to communicate with API")
+            return None
+        except Speech.UnknownValueError:
+            print(f"{self.__class__.__name__} failed to hear the spoken words")
+            return None
+        except KeyboardInterrupt:
+            print(f"{self.__class__.__name__} failed to complete on time")
+            return None
+
     def extract_features(self, document, tokens):
         return {f"contains({word})": word in tokens for word, tag in Text(document).tags if tag in self.configurations["VERB_TAGS"]}
 
@@ -69,6 +154,26 @@ class Smartroom(object):
         self._verbs = value
 
     @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        self._state = value
+        self._state()
+
+    @property
+    def telegram(self):
+        return self._telegram
+
+    @telegram.setter
+    def telegram(self, value):
+        self._telegram = {
+            "command": self.configurations["COMMANDS"]["BAOS"],
+            "value": value
+        }
+
+    @property
     def polarities(self):
         return self._polarities
 
@@ -79,6 +184,14 @@ class Smartroom(object):
     @property
     def response(self):
         return self._response
+
+    @property
+    def classifier(self):
+        return self._classifier
+
+    @classifier.setter
+    def classifier(self, value):
+        self._classifier = value
 
     @property
     def text(self):
--- smartroomtest.robot
+++ smartroomtest.robot
@@ -2,6 +2,46 @@
 Variables       smartroomtest.py
 
 * Test Cases *
-Test
-    ${luna.text}        Set Variable    Please turn on the lights
-    Log To Console      ${luna.nouns}
+TestPerformNaiveBayes1
+    ${luna.text}        Set Variable     please turn on the lights
+    log to console  ${luna.polarities}
+TestPerformNaiveBayes2
+    ${luna.text}        Set Variable     please turn off the lights
+    log to console  ${luna.polarities}
+TestPerformNaiveBayes3
+    ${luna.text}        Set Variable     please don't turn on the lights
+    log to console  ${luna.polarities}
+TestPerformNaiveBayes4
+    ${luna.text}        Set Variable     please do not turn on the lights
+    log to console  ${luna.polarities}
+TestPerformNaiveBayes5
+    ${luna.text}        Set Variable     please turn on and off the lights
+    log to console  ${luna.polarities}
+TestBuildTrainingData1
+    ${luna.text}    set Variable    please turn off the lights
+TestBuildTrainingData2
+    ${luna.text}    set Variable    please turn off the lights
+TestConvertSpeechToText1
+    ${luna.text}    set Variable    please don't turn off the lights
+TestConvertSpeechToText2
+    ${luna.text}    set Variable    please don't turn off the lights
+TestGetmicrophoneindex1
+    ${luna.text}    set Variable    please do not turn on the lights
+TestGetmicrophoneindex2
+    ${luna.text}    set Variable    please do not turn on the lights
+TestPerformClassification1
+    ${luna.text}    set Variable    please do not turn on the lights
+TestPerformClassification2
+    ${luna.text}    set Variable    please do not turn on the lights
+TestPerformRequest1
+    ${luna.text}    set Variable    please do not turn on the lights
+TestPerformRequest2
+    ${luna.text}    set Variable    please do not turn on the lights
+Testwaitforwakeword1
+    ${luna.text}    set Variable    please do not turn on the lights
+    ${luna.wait_for_wake_word}  set Variable    luna
+    Should Be Equal ${luna.wait_for_wake_word}  ${luna.text}
+Testwaitforwakeword2
+    ${luna.text}    set Variable    luna
+    ${luna.wait_for_wake_word}  set Variable    luna
+    Should Be Equal ${luna.wait_for_wake_word}  ${luna.text}
